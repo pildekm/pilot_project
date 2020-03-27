3e434d8c99da282006809663eebb025cb29517d5
dope <jurkaboy@gmail.com>
2020-03-27 09:53:00 +0100

    dodani testovi

---
+++ courses/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from . import models
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = models.Quiz
+        fields = ['title', 'description', 'order', 'total_questions']
+        # exclude = ['course',]
+
+class TrueFalseQuestionForm(forms.ModelForm):
+        class Meta:
+            model = models.TrueFalseQuestion
+            fields = ['order', 'prompt']
+
+class MultipleChoiceQuestionForm(forms.ModelForm):
+    class Meta:
+        model = models.MultipleChoiceQuestion
+        fields = ['order', 'prompt', 'shuffle_answers']
--- courses/urls.py
+++ courses/urls.py
@@ -5,8 +5,9 @@ app_name = "courses"
 
 urlpatterns = [
     url(r'^$', views.course_list, name='Courses'),
-url(r'(?P<course_pk>\d+)/t(?P<step_pk>\d+)$', views.text_detail, name='Text'),
+    url(r'(?P<course_pk>\d+)/t(?P<step_pk>\d+)$', views.text_detail, name='Text'),
     url(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)$', views.quiz_detail, name='Quiz'),
+    url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name='CreateQuiz'),
+    url(r'(?P<course_pk>\d+)/edit_quiz/(?P<quiz_pk>\d+)/$', views.quiz_edit, name='EditQuiz'),
     url(r'(?P<pk>\d+)$', views.course_detail, name='Detail'),
-
-]
+    ]
