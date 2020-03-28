f5e335765f6862bd174e12eb7853727808817c5c
dope <jurkaboy@gmail.com>
2020-03-28 12:20:20 +0100

    dodani testovi

--- courses/forms.py
+++ courses/forms.py
@@ -17,3 +17,9 @@ class MultipleChoiceQuestionForm(forms.ModelForm):
     class Meta:
         model = models.MultipleChoiceQuestion
         fields = ['order', 'prompt', 'shuffle_answers']
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = models.Answer
+        fields = ['order', 'text', 'correct']
--- courses/urls.py
+++ courses/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     url(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)$', views.quiz_detail, name='Quiz'),
     url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name='CreateQuiz'),
     url(r'(?P<course_pk>\d+)/edit_quiz/(?P<quiz_pk>\d+)/$', views.quiz_edit, name='EditQuiz'),
+    url(r'(?P<quiz_pk>\d+)/create_question/(?P<question_type>mc|tf)/$', views.create_question, name='CreateQuestion'),
+    url(r'(?P<quiz_pk>\d+)/edit_question/(?P<question_pk>\d+)/$', views.edit_question, name='EditQuestion'),
+    url(r'(?P<question_pk>\d+)/create_answer/$', views.answer_form, name='CreateAnswer'),
     url(r'(?P<pk>\d+)$', views.course_detail, name='Detail'),
     ]
